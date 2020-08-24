fa1b390c3ec3c5a924b6331b801619132485ff2c
fernancumez <fernancumez@gmail.com>
2020-08-24 15:37:04 -0600

    Create API routes

---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  getTodos,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+} from "../controllers/todos";
+
+const router: Router = Router();
+
+router.get("/api/todos", getTodos);
+
+router.post("/api/add-todo", addTodo);
+
+router.put("/api/edit-todo/:id", updateTodo);
+
+router.delete("/api/delete-todo/:id", deleteTodo);
+
+export default router;
